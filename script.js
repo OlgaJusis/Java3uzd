41375b1ea64a86bfc28921948d7a87e90183e05c
OlgaJusis <olgajusis@gmail.com>
2025-02-26 17:58:03 +0200

    Java3uzd

---
+++ script.js
@@ -0,0 +1,67 @@
+let asmuoNR1 = {
+    vardas: 'Jonas',
+    pavarde: 'Jonaitis',
+    age: 32,
+    ugisCm: 184,
+    SvorisKG: 80,
+    bmi: 23.63,
+    smokes: "no",
+}
+
+
+let asmuoNR2 ={
+    vardas: 'John',
+    pavarde: 'Smith',
+    age: 36,
+    ugisCm: 172,
+    SvorisKG: 80,
+    bmi: 27.04,
+    smokes: 'yes',
+} 
+
+let asmuoNR3 ={
+    vardas: 'Jose',
+    pavarde: 'Garcia',
+    age: 11,
+    ugisCm: 152,
+    SvorisKG: 40,
+    bmi: 17.31,
+    smokes: 'no',
+} 
+
+let asmuoNR4 ={
+    vardas: 'Ivan',
+    pavarde: 'Smirnoff',
+    age: 45,
+    ugisCm: 174,
+    SvorisKG: 120,
+    bmi: 39.64, 
+    smokes: 'yes',
+} 
+
+let asmuoNR5 ={
+    vardas: 'Ian',
+    pavarde: 'Nowak',
+    age: 30,
+    ugisCm: 186,
+    SvorisKG: 150,
+    bmi: 43.36,
+    smokes: 'no',
+    } 
+
+
+console.log(asmuoNR1.vardas);
+console.log(asmuoNR1.age);
+console.log(asmuoNR1.bmi);
+console.log(asmuoNR1.smokes);
+ if (asmuoNR1.age < 30 && bmi < 25 && smokes === no) {
+    console.log(Jusu, rizika, maza. Nepamirškite, sportuoti, ir, sveikai, maitintis. bei, ir, toliau, nerukykite);
+ } else if(asmuoNR1.age >= 30 && age <= 50 && bmi < 25 && bmi <30 && smokes === no) {
+}else if (asmuoNR1.age >= 50 || bmi >= 30 || smokes === yes) {
+    console.log(Jusu, rizika, didele. Sportuokite, ir, sveikai, maitinkites. Meskite, rukyti);
+     } else if(asmuoNR1.age >= 50 && bmi > 30 && bmi <30 && smokes === yes) {
+        console.log(Jusu, rizika, labai,didele. Sportuokite, ir, sveikai, maitinkites. Meskite, rukyti. Uzsiimkite, meditacija);
+ } 
+ else {
+    console.log(Jusu, rizika, neapibrezta. Tyrimas, reikalauja, daugiau, duomenu);
+ }
